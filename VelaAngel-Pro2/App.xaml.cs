9fd7bb0e030e4b9668703c8a2163ff012eab1f7b
Angell50 <angel.vela@udla.edu.ec>
2024-12-03 07:23:36 -0500

    Agregar archivos de proyecto.

---
+++ VelaAngel-Pro2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace VelaAngel_Pro2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ VelaAngel-Pro2/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace VelaAngel_Pro2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
